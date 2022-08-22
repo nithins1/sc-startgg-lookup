316b7bf47a1ea42cffc7f0ad97082ec594cf6dbe
nithins1 <nithin@singhalhome.org>
2022-08-21 18:29:49 -0700

    adding query for tournaments near santa cruz

--- lookupplayers.py
+++ lookupplayers.py
@@ -1,41 +1,45 @@
 import requests
+import argparse
 
+parser = argparse.ArgumentParser(description="Gets list of Santa Cruz players attending a specific event.")
+parser.add_argument("-t", "--tourneys", type=int, metavar="tourneys", default=44, help="number of previous Santa Cruz tourneys to consider")
+
+args = parser.parse_args()
 
 with open('key.txt', 'r') as file:
     api_key = file.read()
 
-query = """query EventEntrants($eventId: ID!, $page: Int!, $perPage: Int!) {
-  event(id: $eventId) {
-    id
-    name
-    entrants(query: {
-      page: $page
-      perPage: $perPage
-    }) {
-      pageInfo {
-        total
-        totalPages
-      }
-      nodes {
-        id
-        participants {
-          id
-          gamerTag
-        }
+sc_events = [f"11th-hour-smash"]
+
+
+tourneys_query = """query SCTournaments($perPage: Int, $coordinates: String!, $radius: String!) {
+  tournaments(query: {
+    perPage: $perPage
+    filter: {
+      location: {
+        distanceFrom: $coordinates,
+        distance: $radius
       }
     }
+  }) {
+    nodes {
+      id
+      name
+      city
+    }
   }
-}
-"""
-
-variables = """{
-  "eventId": 121970,
-  "page": 1,
-  "perPage": 2
 }"""
 
-x = requests.post(url="https://api.start.gg/gql/alpha",
-              json={"query": query, "variables": variables},
+tourneys_variables = f"""{{
+  "perPage": {args.tourneys},
+  "coordinates": "36.9741,-122.0308",
+  "radius": "10mi"
+}}"""
+
+t = requests.post(url="https://api.start.gg/gql/alpha",
+              json={"query": tourneys_query, "variables": tourneys_variables},
               headers={"Authorization": "Bearer " + api_key})
 
-print(x.text)
+tourneys = t.json()["data"]["tournaments"]["nodes"]
+tourney_ids = [t["id"] for t in tourneys]
+print(tourney_ids)
